e040bc663876d8a11d53c9ad6e7803ab085753da
xus898 <xus898@outlook.com>
2021-08-21 00:24:50 +0800

    episode-07

--- src/main/java/com/echo/boot/MainApplication.java
+++ src/main/java/com/echo/boot/MainApplication.java
@@ -2,11 +2,19 @@ package com.echo.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class MainApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MainApplication.class, args);
+        // 返回 IOC 容器
+        ConfigurableApplicationContext run = SpringApplication.run(MainApplication.class, args);
+
+        // 查看容器里的组件
+        String[] names = run.getBeanDefinitionNames();
+        for (String name : names) {
+            System.out.println(name);
+        }
     }
 }
